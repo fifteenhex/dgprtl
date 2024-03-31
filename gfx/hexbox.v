036f2a712d2b25b2dc04a2df4842e93ddc13b00e
Daniel Palmer <daniel@0x0f.com>
2024-03-31 22:12:44 +0900

    hexbox: Make width configurable

--- gfx/hexbox.v
+++ gfx/hexbox.v
@@ -3,17 +3,16 @@ module hexbox
     COLS = 8
 )
 (
-    input logic clk,
-    input logic [31:0] value,
+    input logic [((COLS * 4) - 1):0] value,
     output logic [7:0] chars [0:COLS - 1]
 );
 
 genvar i;
 generate
-    for (i = 0; i < 8; i++) begin : bin2ascii_insts
+    for (i = COLS - 1; i >= 0; i--) begin : bin2ascii_insts
         bin2ascii bin2ascii_inst(
-            .nibble(value[31 - (4 * i): 28 - (4 * i)]),
-            .ascii(chars[i])
+            .nibble(value[(((i + 1) * 4) - 1) : (i * 4)]),
+            .ascii(chars[(COLS - 1) - i])
         );
     end
 endgenerate
